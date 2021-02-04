7f1f61e728023607eecb3f5114b8eb46b871494d
thuglifescientist2018 <thuglifescientist2018@gmail.com>
2021-02-04 14:22:07 +0545

    leaf drop

---
+++ js/app.js
@@ -0,0 +1,8 @@
+let img_container = document.querySelector('.img-container');
+
+function createLeaf() {
+    let leaf = "<div class='leaf'></div>";
+    return leaf;
+}
+
+img_container.innerHTML = createLeaf();
